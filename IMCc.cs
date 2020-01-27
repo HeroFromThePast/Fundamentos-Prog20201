bd266922c7d9c21630b6c97790a0cecc85f3bfd1
Juan José Pérez Sánchez <60188878+HeroFromThePast@users.noreply.github.com>
2020-01-27 15:53:43 -0500

    Add files via upload

---
+++ IMCc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Calculadora indice de masa corporal");
+            Console.WriteLine("Ingrese su estatura en metros");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su peso en kilogramos");
+            double W = double.Parse(Console.ReadLine());
+
+            double IMC = W / (h * h);
+
+            if (IMC < 18.5)
+            {
+                Console.WriteLine("su IMC es de " + IMC + ", está por debajo de lo normal");
+            }
+            else if (18.5 <= IMC && IMC <= 24.9)
+            {
+                Console.WriteLine("su IMC es de " + IMC + ", Normal");
+            }
+            else if (25 <= IMC && IMC <= 29.9) 
+            {
+                Console.WriteLine("su IMC es de " + IMC + ", está por encima de lo normal");
+            }
+            else 
+            {
+                Console.WriteLine("su IMC es de " + IMC + ", Usted está obeso");
+            }
+
+
+        }
+    }
+}
---
+++ ecuacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Resolver la ecuacion cuadrática");
+            Console.WriteLine("ingrese el valor de A");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el valor de B");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el valor de C");
+            double c = double.Parse(Console.ReadLine());
+
+            double disc = (Math.Pow(b, 2) - (4 * a * c));
+
+            if (disc == 0)
+            {
+                double x1 = -b / (2 * a);
+                Console.WriteLine("la solucion existe y es unica ");
+            }
+            else if (disc > 0)
+            {
+                double x1 = (-b + disc) / (2 * a);
+                double x2 = (-b - disc) / (2 * a);
+                Console.WriteLine("Existen dos posibles soluciones correctas");
+                Console.WriteLine("x1= " + x1 + ", x2= " + x2);
+            }
+            else 
+            {              
+                Console.WriteLine("la solucion no existe ");
+            }
+        }
+    }
+}
