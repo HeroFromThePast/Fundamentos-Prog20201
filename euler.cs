e435e74a0e3f2d1d26667275a7baa2fa6c6ba769
Juan José Pérez Sánchez <60188878+HeroFromThePast@users.noreply.github.com>
2020-02-24 15:20:28 -0500

    Add files via upload

---
+++ euler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace eulerprog
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x = 0;
+            double ExpX = 0;
+           // double iFact = factorial(i);
+            Console.WriteLine("ingrese el valor de x");
+            x = double.Parse(Console.ReadLine());
+            
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                double resultadoant = 0;
+                ExpX +=  Math.Pow(x, i) / factorial(i);
+                resultadoant += ExpX;
+                Console.WriteLine(resultadoant);
+
+            }
+           
+        }
+
+        public static double factorial(double number)
+        {
+            if (number == 0) return 1; 
+            if (number == 1) return 1;
+            else return number * factorial(number - 1);
+        }
+    }
+}
---
+++ sin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinprog
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x = 0;
+            double resultado = 0;
+            
+            Console.WriteLine("ingrese el valor de x");
+            x = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double sen = 0;
+                resultado += (Math.Pow(-1, i) / factorial(((2 * i) + 1))) * (Math.Pow(x, (2 * i) + 1));
+                sen += resultado;
+                
+             
+            }
+            Console.WriteLine(sen);
+        }
+
+
+
+
+
+        public static double factorial(double number)
+        {
+            if (number == 0) return 1;
+            if (number == 1) return 1;
+            else return number * factorial(number - 1);
+        }
+    }
+}
