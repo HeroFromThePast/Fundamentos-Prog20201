5e300b1db2038602863ca2a43557fac2e493339d
Juan José Pérez Sánchez <60188878+HeroFromThePast@users.noreply.github.com>
2020-03-06 15:26:11 -0500

    Add files via upload

---
+++ stringAr.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Rep = 0;
+            int wordIndex = 0; 
+
+
+            Console.WriteLine("Escriba una frase");
+            string frase = Console.ReadLine();
+
+            frase = frase.ToUpper();
+            string[] palabras = frase.Split(' ');
+
+            Console.WriteLine("Escriba una palabra de la frase");
+            string palabra = Console.ReadLine();
+            palabra = palabra.ToUpper();
+
+            int[] posiciones = new int[palabras.Length];
+
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                Console.WriteLine(palabras[i]);
+                if (palabras[i] == palabra)
+                {
+                   
+                    Rep += 1;
+                    posiciones[wordIndex] = i;
+                    wordIndex++;
+                   
+                }
+               
+            }
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                Console.WriteLine("la palabra está " + Rep + " veces en las posiciones " + posiciones[i]);
+            }
+
+           
+
+
+        }
+    }
+}
